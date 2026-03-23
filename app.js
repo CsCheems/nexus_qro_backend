a7c426a1643cc3e2b5078647901ebd1efe3f1093
CsCheems <j.dev.mx@outlook.com>
2026-03-23 17:31:37 -0600

    se modifico el cors

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require('express');
 const cookieParser = require("cookie-parser");
 const app = express();
 const cors = require('cors');
-app.use(cors());
+app.use(cors({
+    origin: process.env.ORIGIN,
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
